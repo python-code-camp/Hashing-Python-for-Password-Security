9af27f345964d00a00ebb9db5648890bed8e7dfa
Python-Code-Camp <155484647+python-code-camp@users.noreply.github.com>
2024-02-04 19:09:29 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import argon2
+
+password = "MySecurePassword"
+
+# Create a PasswordHasher object
+ph = argon2.PasswordHasher()
+
+# Hash the password using Argon2
+hashed_password = ph.hash(password)
+
+print("Hashed Password:", hashed_password)
+
+
+# Authenticate the password
+
+input_password = "MySecurePassword"
+
+try:
+    ph.verify(hashed_password, input_password)
+    print("Password verified successfully!")
+except argon2.exceptions.VerifyMismatchError:
+    print("Password verification failed.")
